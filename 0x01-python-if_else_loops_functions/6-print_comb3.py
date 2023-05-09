e021a62c191684145aa49eda8f555d795b5a602b
zaki-prod <zakiprod04@gmail.com>
2023-05-09 15:04:09 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for number in range(1, 90):
-    if number // 10 < number % 10:
-        print("{:02d}".format(number),end='\n' if number == 89 else ",")
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
