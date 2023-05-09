d140097665260293e2616b347d662b6a5016ba75
zaki-prod <zakiprod04@gmail.com>
2023-05-09 13:41:14 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # function that prints the number and thier hexadecimal equivalents
 for i in range(100):
-    print("{} {}".format(i, hex(i)))
+    print("{} {}".format(i, hex(i)[2:]))
