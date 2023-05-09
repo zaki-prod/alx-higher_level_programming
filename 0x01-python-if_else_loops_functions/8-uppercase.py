54403a2304e1751316254734a2308c259ca62144
zaki-prod <zakiprod04@gmail.com>
2023-05-09 15:44:56 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def char_to_uper(character):
+def to_uper(character):
     if ord(character) >= 97 and ord(character) <= 122:
         return (ord(character) - 32)
     else:
         return ord(character)
 
 
-def convert_string_uppercase(str):
+def uppercase(str):
     new = ""
     for character in str:
-        new += "%c" % char_to_uper(character)
+        new += "%c" % to_uper(character)
     print("{:s}".format(new))
