98b5d9ed73bc4bb02d44755ad256b9abcc655fbf
zaki-prod <zakiprod04@gmail.com>
2023-05-09 16:18:08 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-# 12-fizzbuzz.py
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-
-fizzbuzz()
-print("")
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print(f"{num} ", end="")
