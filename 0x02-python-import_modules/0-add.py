f27b90465209fa1a781e91bb78735c651a97c170
zaki-prod <zakiprod04@gmail.com>
2023-05-11 11:37:45 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    x = 1
-    y = 2
-    print("{} + {} = {}".format(x, y, add(x, y)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
