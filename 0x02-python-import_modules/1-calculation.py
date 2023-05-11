c5f953ae6fe91cdfac412f1d73163894ed9bd1ff
zaki-prod <zakiprod04@gmail.com>
2023-05-11 11:43:19 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, ,sub ,mul, div
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("Add: {}".format(result_add))
+print("Sub: {}".format(result_sub))
+print("Mul: {}".format(result_mul))
+print("Div: {}".format(result_div))
