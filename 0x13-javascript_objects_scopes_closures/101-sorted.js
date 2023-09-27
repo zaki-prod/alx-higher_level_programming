f1532845ba729f77378a9a28c3c708bd6aaf0163
zaki-prod <zakiprod04@gamil.com>
2023-09-27 14:23:27 -0700

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const dict = require('./101-data').dict;
+const dKeys = Object.keys(dict);
+const values = Object.values(dict);
+let matched;
+const result = {};
+// loop over the values
+for (let i = 0; i < values.length; i++) {
+  result[JSON.stringify(values[i])] = [];
+  matched = dKeys.filter(key => dict[key] === values[i]);
+  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
+}
+console.log(result)
