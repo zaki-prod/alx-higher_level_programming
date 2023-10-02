2d137d92c1e63d6774078a9b86ff6c4d3ebeb148
zaki-prod <zakiprod04@gamil.com>
2023-10-02 15:02:00 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL and an email, 
-sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
+"""a Python script that takes in a URL
+sends a request to the URL and 
+
+displays the value of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import urllib.request
